3be1cf38fc4c170c35c72ddf8d6621ae00e004e7
Akshat Goyal <goyalakshat111@gmail.com>
2025-04-23 07:37:24 +0530

    dynamic fields

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
-import './../styles/App.css';
+import React,{useState} from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [fields, setFields] = useState([
+    {
+      name: "",
+      age: "",
+    },
+  ]);
 
-export default App
+  function addField() {
+    setFields([...fields, { name: "", age: "" }]);
+  }
+
+  function removeFields(index) {
+    setFields(fields.filter((_, i) => i !== index));
+  }
+
+  function handleChange(index,e){
+    const updatedFields = [...fields]; // shallow copy of the array
+    updatedFields[index][e.target.name] = e.target.value; // update the specific field
+    setFields(updatedFields); // set the new array
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(fields);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      {fields.map((field, index) => (
+        <div key={index}>
+          <input type="text" name="name" placeholder="Name"
+          value={field.name}
+           onChange={(e)=>{handleChange(index,e)}}/>
+          <input type="number" name="age" placeholder="Age" 
+          value={field.age}
+          onChange={(e)=>{handleChange(index,e)}} />
+          <button type="button" onClick={()=>removeFields(index)}>Remove</button>
+        </div>
+      ))}
+      <button type="button" onClick={addField}>Add More...</button>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default App;
